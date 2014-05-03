8b2c3c4d589853f401dcfc6d414d441362f8600f
Carles Climent <carlescliment@hotmail.com>
2014-05-03 17:34:08 +0200

    Added constructor to SfEvent class. EventInterface now implements the method getMessage()

--- spec/carlescliment/MudClient/Adapter/SfEventBuilderSpec.php
+++ spec/carlescliment/MudClient/Adapter/SfEventBuilderSpec.php
@@ -9,9 +9,10 @@ class SfEventBuilderSpec extends ObjectBehavior
 {
     function it_builds_instances_of_symfony_event()
     {
-        $event = $this->buildFor('some_message');
+        $event = $this->buildFor('some message');
 
         $event->shouldbeAnInstanceOf('Symfony\Component\EventDispatcher\Event');
         $event->shouldbeAnInstanceOf('carlescliment\MudClient\Event\EventInterface');
+        $event->getMessage()->shouldBe('some message');
     }
 }
--- src/carlescliment/MudClient/Adapter/SfEvent.php
+++ src/carlescliment/MudClient/Adapter/SfEvent.php
@@ -8,5 +8,16 @@ use carlescliment\MudClient\Event\EventInterface;
 
 class SfEvent extends Event implements EventInterface
 {
+    private $message;
 
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
 }
